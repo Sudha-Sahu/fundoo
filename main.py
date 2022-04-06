288d40eadb927d6b8544e2c141ef0c0a160cae93
Sudha Sahu <sudhasahu52@gmail.com>
2022-04-06 15:16:29 +0530

    [Sudha]-refactored the logic in user login project and added class based apis for reset password and forget password

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from flask import Flask, request, session, make_response, jsonify
+
+from flask import Flask, request, session
 from mongoengine import connect, ValidationError, NotUniqueError
 from flask_restful import Resource, Api
 import json
 from model import User
 import jwt
+from util import activate_my_email
 
 connect(host='mongodb://127.0.0.1:27017/FlaskUserProject')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'af6263f9c9984fd78fb242775905d95f'
 api = Api(app)
 
 
 class UserRegistration(Resource):
-    def get(self, id_):
-        data = request.args
-        user_name = data.get('user_name')
-        password = data.get('password')
-        print('data get', id_, user_name, password)
-        return {'msg': 'you are getting data successfully'}
-
-    def post(self, id_):
+    def post(self):
         data = json.loads(request.data)
-        print('body_data', data)
         first_name = data.get('first_name')
         last_name = data.get('last_name')
         user_name = data.get('user_name')
@@ -33,16 +28,19 @@ class UserRegistration(Resource):
         if conf_password != password:
             return {'error': 'password did not matched'}
         try:
-            new_user = User(user_name=user_name, password=password, first_name=first_name, last_name=last_name, email=email, gender=gender)
+            new_user = User(user_name=user_name,
+                            password=password,
+                            first_name=first_name,
+                            last_name=last_name,
+                            email=email,
+                            gender=gender)
             new_user.save()
             token = jwt.encode({"user_id": new_user.id, 'user_name': new_user.user_name}, "secret", algorithm="HS256")
             url = f'http://127.0.0.1:9090//activate?token={token}'
-            activate_my_email(email, url, user_name)
+            activate_my_email(email, url, new_user.id)
         except ValidationError as e:
-            print(e.to_dict())
             return {'error': e.to_dict()}
         except NotUniqueError as e:
-            print(e)
             return {'error': str(e)}
 
         return {'msg': 'new user added successfully', 'token_activate_url': url}
@@ -58,14 +56,11 @@ class LoginUser(Resource):
             return {'error': 'user not found'}
         if not new_user.is_active:
             return {'error': 'You have to verify email'}
-        print('password', new_user.password)
-        print('user', new_user)
         if password != new_user.password:
             return {'error': 'password not matching'}
         token = jwt.encode({"user_id": new_user.id, 'user_name': new_user.user_name}, "secret", algorithm="HS256")
         decode_ = jwt.decode(token, "secret", algorithms=["HS256"])
         print('decoded', decode_)
-        print('data get', data.get('user_name'))
         return {'msg': 'you are logged in....', 'token': token}
 
 
@@ -74,7 +69,7 @@ class LogoutUser(Resource):
         user_id = session['id']
         session['logged_in'] = False
         print(user_id, "logged out")
-        return make_response(jsonify(message='Logged Out'), 200)
+        return {'msg': 'Logged Out', 'code': 200}
 
 
 class ActivateAccount(Resource):
@@ -100,32 +95,28 @@ class ForgetPassword(Resource):
         print(f'{user_name} has been forgotten their password')
         token = request.args.get('token')
         print(token)
+        activate_my_email(email, )
         return {'msg': 'Check your Registered Mail ID to set new Password....'}
 
 
 class ResetPassword(Resource):
     def post(self):
         if session['logged_in']:
-            user_name = request.form.get('UserName')
-            old_pass = request.form.get('Old Password')
-            new_password1 = request.form.get('New Password')
-            conf_password2 = request.form.get('Re-Enter New Password')
-            if new_password1 == conf_password2:
-                data = User.objects(user_name=user_name).first()
-                if old_pass == data.password:
-                    data.update(password=new_password1)
-                    print(f'{user_name} changed his password')
-                    return {'msg': 'Your Password is Updated.'}
-                else:
-                    return {'msg': 'Check Your old Password'}
+            user_name = request.form.get('user_name')
+            last_pass = request.form.get('old_password')
+            new_password1 = request.form.get('new_password')
+            conf_new_password = request.form.get('Re-Enter New Password')
+            if new_password1 == conf_new_password:
+                print(f'{user_name} changed his password')
+                return {'msg': 'Your Password is Updated.'}
             else:
                 return {'msg': 'Re-Entered Password must be equal to New_Password'}
         else:
-            return {'msg': 'You have to Login First'}
+            return {'msg': 'user please Login First'}
 
 
 api.add_resource(LoginUser, '/login/')
-api.add_resource(UserRegistration, '/register/<int:id_>')
+api.add_resource(UserRegistration, '/register/')
 api.add_resource(ActivateAccount, '/activate/')
 api.add_resource(LogoutUser, '/logout/')
 api.add_resource(ForgetPassword, '/forgetpassword/')
@@ -133,4 +124,3 @@ api.add_resource(ResetPassword, '/resetpassword/')
 
 if __name__ == "__main__":
     app.run(debug=True, port=9090)
-
