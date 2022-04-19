e93b17142a76fa00d743b2f4afb88ceef86e5442
Sudha Sahu <sudhasahu52@gmail.com>
2022-04-19 10:40:17 +0530

    [Sudha]-added docstring to each apis and refactored the label part

--- notes/notes_apis.py
+++ notes/notes_apis.py
@@ -1,12 +1,18 @@
-from flask import request, session
+from flask import request
 from mongoengine import ValidationError
 from flask_restful import Resource
 import json
 from .model import Notes
-from labels.model import Label
 import datetime
 from .utils import token_required
-from .validators import validate_new_note
+from labels.model import Label
+import redis
+
+
+r = redis.Redis(
+    host='27017',
+    port=9090,
+    password='password')
 
 
 class NoteAPI(Resource):
@@ -17,19 +23,28 @@ class NoteAPI(Resource):
             @param request: It takes note-title, description, label(optional)
             @return: creates successful user notes
         """
+        user_id = self.get('_id')
+        data = json.loads(request.data)
+        title = data.get('title')
+        desc = data.get('desc')
+        color = data.get('color')
+        label = data.get('label')
+        note_created = datetime.datetime.now()
+        notes = Notes(title=title,
+                      desc=desc,
+                      user_id=user_id,
+                      color=color,
+                      note_created=note_created)
+        notes.save()
+        print(label)
         try:
-            user_id = self.get('_id')
-            data = json.loads(request.data)
-            title = data.get('title')
-            desc = data.get('desc')
-            color = data.get('color')
-            #label_id = data.get('label_id')
-            note_created = datetime.datetime.now()
-            notes = Notes(title=title,
-                          desc=desc,
-                          user_id=user_id,
-                          color=color,
-                          note_created=note_created)
+            labl = Label.objects.filter(label=label, user_id=user_id)
+            if labl:
+                notes.label_id.append(labl.first())
+            else:
+                labl_ = Label(label=label, user_id=user_id)
+                labl_.save()
+                notes.label_id.append(labl_)
             notes.save()
             return {'msg': 'User added new notes', 'code': 200}
         except Exception:
@@ -44,24 +59,22 @@ class NoteAPI(Resource):
         user_id = self.get('_id')
         note_filter = request.args.get('filter')
         print(note_filter)
-
         all_notes = []
 
         try:
-
             if note_filter == 'pinned':
                 my_notes = Notes.objects.filter(user_id=user_id, is_trash=False, is_pinned=True)
             elif note_filter == "trash":
                 my_notes = Notes.objects.filter(user_id=user_id, is_trash=True).order_by("-is_pinned")
             else:
                 my_notes = Notes.objects.filter(user_id=user_id, is_trash=False).order_by("-is_pinned")
+
             if not my_notes:
                 return {'msg': 'NO NOTE PRESENT IN DATABASE', 'error code': 400}
 
             for note in my_notes:
                 dict_itr = note.to_dict()
                 all_notes.append(dict_itr)
-            #print(all_notes)
         except TypeError as e:
             return {'Error': f" {e} didn't find any notes"}
         return {'notes': all_notes}
@@ -80,12 +93,8 @@ class EditNotes(Resource):
             note_id = kwargs['note_id']
             user_id = self.get('_id')
             note = Notes.objects.filter(id=note_id, user_id=user_id).first()
-            updated_data = request.form
+            updated_data = json.loads(request.data)
             note.update(**updated_data)
-            label = Label.objects.filter(user_id=user_id, id=note.label_id).first()
-            if not label:
-                label = Label(user_id=user_id, id=note.label_id)
-                label.save()
 
         except Exception as e:
             return {'Error': str(e), 'code': 500}
@@ -102,9 +111,11 @@ class EditNotes(Resource):
             note_id = kwargs['note_id']
             user_id = self.get('_id')
             note = Notes.objects.filter(id=note_id,  user_id=user_id).first()
-            if note:
+            if not note.is_trash:
+                return {"msg": "note not found in trash"}
+            if note.is_trash:
                 note.delete()
-            return {'message': 'Notes Deleted', 'code': 200}
+                return {'message': 'Notes Deleted', 'code': 200}
         except Exception:
             return {'msg': "note not found"}
 
@@ -117,6 +128,7 @@ class EditNotes(Resource):
         """
         note_id = kwargs['note_id']
         user_id = self.get('_id')
+        print(note_id, user_id)
         notes = Notes.objects.filter(id=note_id, user_id=user_id).first()
         try:
             if notes['id'] == note_id:
@@ -128,6 +140,7 @@ class EditNotes(Resource):
                     'is_pinned': notes['is_pinned'],
                     'is_trash': notes['is_trash'],
                     'color': notes['color'],
+                    'label_id': [lb.label for lb in notes.label_id],
                     'date_created': str(notes['note_created'])
                 }
         except Exception:
@@ -196,26 +209,4 @@ class TrashNotes(Resource):
                 }
         except Exception as e:
             return {'msg': f'{e} some error occured'}
-"""
 
-class LabelNoteAPI(Resource):
-    @token_required
-    def post(self,  *args, **kwargs):
-        
-            This API is used to add a note to a label
-            @param request: label id
-            @return: adds label id to the note
-        
-        data = json.loads(request.data)
-        label_id = data.get('label_id')
-        lb = Label.objects.filter(id=label_id).first()
-        user_id = self.get('_id')
-        notes = Notes.objects.get(id=user_id)
-        try:
-            if notes[user_id] != user_id:
-                return {'msg': 'Users ids does not match', 'status code': 400}
-            if notes['user_id'] == user_id:
-                pass
-        except:
-            return
-"""
